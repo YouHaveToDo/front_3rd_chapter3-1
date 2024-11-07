9d3cbafd8d5f744dd6300672d8eacd39e84193a3
YouHaveToDo <zmfpdkwl2@gmail.com>
2024-11-08 01:15:24 +0900

    test: medium 테스트 작성

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -1,25 +1,187 @@
-import { act, renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import { http, HttpResponse } from 'msw';
+import { describe, expect, vi } from 'vitest';
 
 import {
-  setupMockHandlerCreation,
   setupMockHandlerDeletion,
   setupMockHandlerUpdating,
 } from '../../__mocks__/handlersUtils.ts';
+import { events } from '../../__mocks__/response/events.json' assert { type: 'json' };
 import { useEventOperations } from '../../hooks/useEventOperations.ts';
 import { server } from '../../setupTests.ts';
 import { Event } from '../../types.ts';
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {});
+const mockToast = vi.fn();
+vi.mock('@chakra-ui/react', () => {
+  const actual = vi.importActual('@chakra-ui/react');
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+  return {
+    ...actual,
+    useToast: () => mockToast,
+  };
+});
 
-it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
+describe('useEventOperations', () => {
+  const mockEvents = [...events] as Event[];
 
-it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {});
+  const title = '테스트 코드를 위한 타이틀';
+  const endTime = '22:00';
 
-it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {});
+  const newEvent = {
+    id: '1',
+    title: title,
+    date: '2024-07-02',
+    startTime: '09:00',
+    endTime: endTime,
+    description: '기존 팀 미팅',
+    location: '회의실 B',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  } as Event;
 
-it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {});
+  it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
+    const { result } = renderHook(() => useEventOperations(false, () => {}));
 
-it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {});
+    await waitFor(() => {
+      expect(result.current.events).toEqual(events);
+    });
+  });
+
+  it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {
+    const { result } = renderHook(() => useEventOperations(false, () => {}));
+
+    await waitFor(() => {
+      expect(result.current.events).toHaveLength(1);
+    });
+
+    const newEvent = {
+      id: '4',
+      title: 'EVENT 4',
+      date: '2024-07-01',
+      startTime: '12:00',
+      endTime: '13:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    } as Event;
+
+    await waitFor(() => {
+      result.current.saveEvent(newEvent);
+    });
+
+    await waitFor(() => {
+      expect(result.current.events).toHaveLength(2);
+    });
+  });
+
+  it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {
+    setupMockHandlerUpdating(mockEvents);
+
+    const { result } = renderHook(() => useEventOperations(true, () => {}));
+
+    await waitFor(() => {
+      expect(result.current.events[0].title).toBe(events[0].title);
+      expect(result.current.events[0].endTime).toBe(events[0].endTime);
+    });
+
+    await waitFor(() => {
+      result.current.saveEvent(newEvent);
+    });
+
+    await waitFor(() => {
+      expect(result.current.events[0].title).toBe(title);
+      expect(result.current.events[0].endTime).toBe(endTime);
+    });
+  });
+
+  it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {
+    setupMockHandlerDeletion(mockEvents);
+
+    const { result } = renderHook(() => useEventOperations(false, () => {}));
+
+    await waitFor(() => {
+      console.log('result 입니다', result.current.events);
+      expect(result.current.events).toHaveLength(1);
+    });
+
+    await waitFor(() => {
+      result.current.deleteEvent('1');
+    });
+
+    await waitFor(() => {
+      expect(result.current.events).toHaveLength(0);
+    });
+  });
+
+  it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {
+    server.use(
+      http.get('/api/events', () => {
+        return HttpResponse.error();
+      })
+    );
+
+    const { result } = renderHook(() => useEventOperations(false, () => {}));
+
+    await waitFor(() => {
+      expect(result.current.events).toHaveLength(0);
+    });
+
+    await waitFor(() => {
+      result.current.fetchEvents();
+    });
+
+    await waitFor(() => {
+      expect(mockToast).toHaveBeenCalledWith({
+        title: '이벤트 로딩 실패',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    });
+  });
+
+  it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {
+    server.use(
+      http.put('/api/events/:id', () => {
+        return HttpResponse.error();
+      })
+    );
+
+    const { result } = renderHook(() => useEventOperations(true, () => {}));
+
+    await waitFor(() => {
+      result.current.saveEvent(newEvent);
+    });
+
+    expect(mockToast).toHaveBeenCalledWith({
+      title: '일정 저장 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+
+  it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {
+    server.use(
+      http.delete('/api/events/:id', () => {
+        return HttpResponse.error();
+      })
+    );
+
+    const { result } = renderHook(() => useEventOperations(false, () => {}));
+
+    await waitFor(() => {
+      result.current.deleteEvent('1');
+    });
+
+    expect(mockToast).toHaveBeenCalledWith({
+      title: '일정 삭제 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+});
--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -1,14 +1,88 @@
 import { act, renderHook } from '@testing-library/react';
+import { beforeEach, describe } from 'vitest';
 
+import { events } from '../../__mocks__/response/events.json' assert { type: 'json' };
 import { useNotifications } from '../../hooks/useNotifications.ts';
 import { Event } from '../../types.ts';
-import { formatDate } from '../../utils/dateUtils.ts';
-import { parseHM } from '../utils.ts';
 
-it('초기 상태에서는 알림이 없어야 한다', () => {});
+describe('useNotifications', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date('2024-10-15T08:50:01'));
+  });
 
-it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+  afterEach(() => {
+    vi.useRealTimers();
+  });
 
-it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
+  const mockEvents = [...events] as Event[];
 
-it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+  it('초기 상태에서는 알림이 없어야 한다', () => {
+    const { result } = renderHook(() => useNotifications(mockEvents));
+
+    expect(result.current.notifications).toEqual([]);
+  });
+
+  it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {
+    const { result } = renderHook(() => useNotifications(mockEvents));
+
+    act(() => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    act(() => {
+      expect(result.current.notifications[0]).toMatchObject({
+        id: '1',
+        message: '10분 후 기존 회의 일정이 시작됩니다.',
+      });
+    });
+  });
+
+  it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {
+    const { result } = renderHook(() => useNotifications(mockEvents));
+
+    act(() => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    act(() => {
+      expect(result.current.notifications[0]).toMatchObject({
+        id: '1',
+        message: '10분 후 기존 회의 일정이 시작됩니다.',
+      });
+    });
+
+    act(() => {
+      result.current.removeNotification(0);
+    });
+
+    expect(result.current.notifications).toEqual([]);
+  });
+
+  it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {
+    const { result } = renderHook(() => useNotifications(mockEvents));
+
+    act(() => {
+      expect(result.current.notifiedEvents).toEqual([]);
+    });
+
+    act(() => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    act(() => {
+      expect(result.current.notifications[0]).toMatchObject({
+        id: '1',
+        message: '10분 후 기존 회의 일정이 시작됩니다.',
+      });
+    });
+
+    act(() => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    act(() => {
+      expect(result.current.notifiedEvents).toHaveLength(1);
+    });
+  });
+});
